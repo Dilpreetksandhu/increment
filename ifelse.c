998a83bfe3392c137ad2bd0a497f66a619a42074
Dilpreetksandhu <74319877+Dilpreetksandhu@users.noreply.github.com>
2022-08-25 20:21:47 +0530

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+
+int salary;
+printf("Enter salary");
+scanf("%d",&salary);
+
+if(salary>=35000){
+printf("You are eligible for 15 percent increment");
+}
+else{
+printf("You are not eligible for 15 percent increment");
+}
+}
+
